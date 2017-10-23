86708b77935049bd9d7c7bee920e9c6d70e8ab40
kasia witwicka <kasia.witwicka@gmail.com>
2017-10-23 21:21:43 +0200

    navbar added and redirects added and home page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :sessions, only: [:new, :create]
   resource :sessions, only: [:destroy]
   resources :users, only: [:new, :create]
