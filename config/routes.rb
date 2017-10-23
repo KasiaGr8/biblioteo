bc02438b24bdfe1ed11b6058c3597457ee171eaf
kasia witwicka <kasia.witwicka@gmail.com>
2017-10-23 22:10:34 +0200

    fixed sign out

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,3 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :profile, only: [:show, :edit, :update] # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
